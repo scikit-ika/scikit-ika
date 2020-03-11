e7a5579694c0ca7e17b0620ab4fa5a549f6fd527
tlac980 <thomas.lacombe@auckland.ac.nz>
2020-03-11 23:14:37 +1300

    Fixed stream to generate the right number of drifts (the widths if the drifts were 'pushing' them) The length of the stream is now calculated from the number of drifts, the period between drifts and the widths of th drifts.

--- skika/data/bernoulli_stream.py
+++ skika/data/bernoulli_stream.py
@@ -20,19 +20,18 @@ class BernoulliStream():
          The stream is simulating the error rate of a learner.
          It is possible to generate drifts by changing the mean error rate at change points.
          
-         Change points are generated regularly along the stream, based on the number of samples and the number of drifts. 
-         A drift is generated every n_samples/n_drifts instances. 
-         
+         Change points are generated regularly along the stream, every drift_period instances. 
+
          The width of the drifts can be specified. If only one value is given, it is applied to every drift. If several ones are specified,
          they are uniformly reparted and randomly applied to the drifts. 
          
-         The new mean error rate after each drift is randomly picked in the mean_errors list.
+         The new mean error rate after each drift is picked in the mean_errors list.
          
          It is possible to retrive drift chracteristics from the stream : magnitude and severity of drifts. 
          
          Arguments :
-            n_samples : int
-                Number of instances to generate
+            drift_period : int
+                Number of instances between two drifts
              
             n_drifts : int
                 Number of drifts to generate
@@ -40,8 +39,14 @@ class BernoulliStream():
             widths_drifts : list of int
                 Width(s) to be applied to the drifts. 
                 
-            mean_errors : list of float
-                List of mean errors to simulation the concepts. Must have at least 2 values to simulate drifts from 1 comcept to another.
+            mean_errors : list of float, or list of lists of floats
+                List of mean errors to simulation the concepts. Can either be : 
+                    - List of mean errors values. The stream will than radomly pair the values to create stable drift periods patterns.
+                      Must have at least 2 values to simulate drifts from 1 comcept to another.
+                      Ex : mean_errors = [0.1,0.2,0.5,0.6]
+                      
+                    - List of lists. Each of the sub-lists must be a pair of two different error rates reprensenting one stable drift period.
+                      Ex : mean_errors = [[0.1,0.2],[0.5,0.6],[0.8,0.9]]
             
             n_stable_drifts : int, (optional, default = 1)
                 Number of floowing drfits with the same patterns (width and mean_errors)
@@ -51,15 +56,14 @@ class BernoulliStream():
         
         """
     def __init__(self,
-                 n_samples,
+                 drift_period,
                  n_drifts,
                  widths_drifts,
                  mean_errors,
                  n_stable_drifts = 1,
                  random_state=0):
     
-        
-        self.n_samples = n_samples
+        self.drift_period = drift_period
         self.n_drifts = n_drifts
         self.widths_drifts = widths_drifts
         self.mean_errors = mean_errors
@@ -69,28 +73,44 @@ class BernoulliStream():
         self._random_state = check_random_state(self.random_state)
         self.r = np.random.RandomState(self.random_state)
         
-        self.stable_period = int(self.n_samples/(self.n_drifts+1))
+#        self.stable_period = int(self.n_samples/(self.n_drifts+1))
         
         
-        # Generate the list of mean_errors based on the number of drifts and the number of drifts (n_drifts) in a stable period (n_stable_drifts)
-        list_err = self.choiceWithoutRepet(math.ceil(self.n_drifts/self.n_stable_drifts)+1,self.mean_errors)
+        if type(self.mean_errors[0]) == list :
+            list_err = self.mean_errors    
+        else :
+            # Generate the list of mean_errors based on the number of drifts and the number of drifts (n_drifts) in a stable period (n_stable_drifts)
+            list_err = self.choiceWithoutRepet(math.ceil(self.n_drifts/self.n_stable_drifts)+1,self.mean_errors)
+            
         self.mean_error_concepts = []
         
-        if len(self.mean_errors) <= 2 :
+        if (type(self.mean_errors[0]) != list and len(self.mean_errors) <= 2) or (type(self.mean_errors[0]) == list and len(self.mean_errors) <= 1):
             alt = 0
             for d in range(self.n_drifts+1):
                 if alt == 0 :
-                    self.mean_error_concepts.append(list_err[0])
+                    if type(self.mean_errors[0]) == list :
+                        self.mean_error_concepts.append(list_err[0][0])
+                    else :
+                        self.mean_error_concepts.append(list_err[0])
                     alt = 1
                 else :
-                    self.mean_error_concepts.append(list_err[1])
+                    if type(self.mean_errors[0]) == list :
+                        self.mean_error_concepts.append(list_err[0][1])
+                    else :
+                        self.mean_error_concepts.append(list_err[1])
                     alt = 0
                 
         else :
             ind_err = 0
             alt = 0 
-            e1 = list_err[ind_err]
-            e2 = list_err[ind_err+1]
+            
+            if type(self.mean_errors[0]) == list :
+                e1 = list_err[ind_err][0]
+                e2 = list_err[ind_err][1]
+            else :
+                e1 = list_err[ind_err]
+                e2 = list_err[ind_err+1]
+                
             for d in range(self.n_drifts):
                 
                 if alt == 0 :
@@ -100,10 +120,20 @@ class BernoulliStream():
                     self.mean_error_concepts.append(e2)
                     alt = 0
                 
+                # Change error rates if n_stable_drifts reached
                 if (((d+1) % self.n_stable_drifts) == 0) and ((d+1) < self.n_drifts) :
                     ind_err += 1
-                    e1 = list_err[ind_err]
-                    e2 = list_err[ind_err+1]
+                    
+                    if type(self.mean_errors[0]) == list :
+                        e1 = list_err[ind_err][0]
+                        e2 = list_err[ind_err][1]
+                        
+                        if ind_err == len(list_err)-1 :
+                            ind_err = -1
+                    else :
+                        e1 = list_err[ind_err]
+                        e2 = list_err[ind_err+1]
+                        
                     alt = 1
                 
             self.mean_error_concepts = np.array(self.mean_error_concepts+[self.mean_error_concepts[-2]])
@@ -117,12 +147,16 @@ class BernoulliStream():
             self.list_widths = self.list_widths[:self.n_drifts]
         else:
             self.list_widths = self.widths_drifts*self.n_drifts
-            
+        
         self.current_width = None
         
+        # Calculate the number of samples there will be in the stream 
+        self.n_samples = (self.n_drifts * self.drift_period) + np.sum(self.list_widths[:]) + self.drift_period
+        
         self.list_positions = []
-        self.position = int(self.n_samples/(self.n_drifts+1))
-        self.list_positions = [(drift+1)*self.position for drift in range(self.n_drifts)]
+        self.position = self.drift_period
+#        self.list_positions = [int((drift+1)*self.position) for drift in range(self.n_drifts)]
+        self.list_positions = [int(((drift+1)*self.position)+np.sum(self.list_widths[:drift])) for drift in range(self.n_drifts)]
         
         # Manual meta-k base
         self.current_drift_severity = None
@@ -315,7 +349,11 @@ class BernoulliStream():
 #            self.current_drift_magnitude = self.list_magnitudes[self.error_rates_combin[0].index((self.current_mean_error,self.next_mean_error))]
 #        except ValueError :
 #            self.current_drift_magnitude = self.list_magnitudes[self.error_rates_combin[1].index((self.current_mean_error,self.next_mean_error))]
-
+        
+        
+#        # TODO :debug
+#        self.list_x = []
+#        self.list_probdrift = []
     
     def next_sample(self, batch_size = 1) :
         
@@ -339,6 +377,8 @@ class BernoulliStream():
             the batch_size samples that were requested.
         """
         
+        
+        
         self.current_prediction = np.zeros(batch_size)
         
         for j in range(batch_size):
@@ -353,14 +393,19 @@ class BernoulliStream():
                 
             self.current_prediction[j] = pred
             
+#            # TODO :debug
+#            self.list_x.append(x)
+#            self.list_probdrift.append(probability_drift)
+            
             # Update theoritical meta-features at every exact drift point
             if self.total_sample_idx in self.list_positions:
                 # Manual meta-knowledge
                 self.current_drift_severity = self.list_widths[self.cpt_drifts]
                 self.current_drift_magnitude = np.sqrt((np.sqrt(self.mean_error_concepts[self.cpt_drifts])-np.sqrt(self.mean_error_concepts[self.cpt_drifts+1]))**2)
                 self.cpt_drifts +=1
-                
-            if (self.sample_idx >= self.stable_period + self.current_width) and (self.concept_idx<self.n_drifts-1):
+            
+            # Update concept
+            if (self.sample_idx >= self.drift_period + self.current_width) and (self.concept_idx<self.n_drifts-1):
                 self.sample_idx = 0
                 
                 self.width_idx += 1
@@ -371,19 +416,18 @@ class BernoulliStream():
                 
                 self.current_width = self.list_widths[self.width_idx]
                 
-#                try :
-#                    self.current_drift_magnitude = self.list_magnitudes[self.error_rates_combin[0].index((self.current_mean_error,self.next_mean_error))]
-#                except ValueError :
-#                    self.current_drift_magnitude = self.list_magnitudes[self.error_rates_combin[1].index((self.current_mean_error,self.next_mean_error))]
-#                
 
-            elif (self.sample_idx >= self.stable_period + self.current_width) and (self.concept_idx>=self.n_drifts-1) : 
+            elif (self.sample_idx >= self.drift_period + self.current_width) and (self.concept_idx>=self.n_drifts-1) : 
                 self.sample_idx = 0
                 self.concept_idx += 1
                 self.current_mean_error = self.mean_error_concepts[self.concept_idx]
                 
             
         self.total_sample_idx += batch_size
+        
+#        # TODO :debug
+#        if self.total_sample_idx == self.n_samples :
+#            print('stop')
         
         return self.current_prediction
     
@@ -457,20 +501,34 @@ class BernoulliStream():
 ####################################################################
 ## Test
 #from scipy.stats import kurtosis, skew
-#     
+     
 #from scipy.spatial import distance
-#
+
 #mag_theo = [0.632,0.578,0.447,0.389,0.289,0.227,0.142,0.067]
 #sev_theo = [1,100,500]
-#
+
 #meta_features = list(itertools.product(sev_theo,mag_theo))
 #
-#n_smpl = 500000
-#stream = BernouilliStream(n_samples=n_smpl, n_drifts = 500, widths_drifts = [1,100,500], mean_errors = [0.1,0.3,0.6,0.9], n_stable_drifts = 6)
+
+#stream = BernoulliStream(drift_period=1500, n_drifts = 500, widths_drifts = [1,50,250], mean_errors = [[0.0,1.0],[0.35,0.45],[0.85,0.4],[0.5,0.2],[0.9,0.3],[0.55,0.75],[0.5,0.1],[0.65,0.75],[0.95,0.25],[0.1,0.0]], n_stable_drifts = 10)
 #stream.prepare_for_use()
 #
 #drift_positions = stream.list_positions
+#n_smpl = stream.n_samples
 #
-
-    
+#list_smpl = []
+#list_mag = []
+#list_sev = []
+#list_width = []
+#list_errors = [[],[]]
+#
+#for i in range(n_smpl) :
+#    list_smpl.append(stream.next_sample()[0])
+#    
+#    if i in drift_positions :
+#        list_mag.append(stream.current_drift_magnitude)
+#        list_sev.append(stream.current_drift_severity)
+#        list_width.append(stream.current_width)
+#        list_errors[0].append(stream.current_mean_error)
+#        list_errors[1].append(stream.next_mean_error)
 
